0ff48893b456f913c810885c25b5ce4b246d6a90
Hlyang <Hlyang7036@gmail.com>
2019-11-27 00:09:48 +0900

    MemberController 추가, MemberRepository 추가, MemberServiceImpl추가, MemberService추가

---
+++ boardEx/src/main/java/com/example/board/repository/MemberService.java
@@ -0,0 +1,9 @@
+package com.example.board.repository;
+
+import com.example.board.domain.Member;
+
+public interface MemberService {
+
+	Member getMember(Member member);
+
+}
---
+++ boardEx/src/main/java/com/example/board/repository/MemberServiceImpl.java
@@ -0,0 +1,22 @@
+package com.example.board.repository;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.board.domain.Member;
+
+public class MemberServiceImpl implements MemberService {
+	
+	@Autowired
+	private MemberRepository memberRepo;
+	
+	@Override
+	public Member getMember(Member member) {
+		Optional<Member> findMember = memberRepo.findById(member.getId());
+		if (findMember.isPresent()) 
+			return findMember.get();
+		
+		else return null;
+	}
+}
